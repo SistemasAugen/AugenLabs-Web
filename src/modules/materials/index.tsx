652fa8686f23be84d9086de96ebb02ca340bad8a
SistemasAugen <sistemas@augenlabs.com>
2022-12-27 17:37:21 -0600

    add routes of new components to index

--- src/modules/materials/index.tsx
+++ src/modules/materials/index.tsx
@@ -1,4 +1,22 @@
 import Materiales from "./Materiales/Materiales";
 import NuestrosMateriales from "./Materiales/NuestrosMateriales/NuestrosMateriales";
 
-export { Materiales, NuestrosMateriales };
+import Parasol from "./Parasol/Parasol";
+import Transforma from "./Parasol/Transforma/Transforma";
+
+import Bblock from "./Bblock/Bblock";
+import NuevaTecnologia from "./Bblock/NuevaTecnologia/NuevaTecnologia";
+
+import Trivex from "./Trivex/Trivex";
+import Reescribiendo from "./Trivex/Reescribiendo/Reescribiendo";
+
+export {
+  Materiales,
+  NuestrosMateriales,
+  Parasol,
+  Transforma,
+  Trivex,
+  Reescribiendo,
+  Bblock,
+  NuevaTecnologia
+};
