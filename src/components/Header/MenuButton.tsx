09bb2f9dfbe2400b4eaf08d2e7f1bd716a1aee00
SistemasAugen <sistemas@augenlabs.com>
2023-01-10 16:57:51 -0600

    change font size of materials menu

--- src/components/Header/MenuButton.tsx
+++ src/components/Header/MenuButton.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Button, Menu, MenuItem, Typography } from '@mui/material';
 
 import Link from "next/link";
+import useStyles from './Header.styles';
 
 const MenuButton = ({ menu }: { menu: any }) => {
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(false);
   const handleOpen = () => {
     setAnchorEl(true);
@@ -19,6 +21,7 @@ const MenuButton = ({ menu }: { menu: any }) => {
         aria-controls={`${menu.name}-menu`}
         aria-haspopup="true"
         onClick={handleOpen}
+        className={[classes.link, classes.contact_link, classes.menu_link_materials].join(" ")}
       >
         {menu.name}
       </Button>
@@ -34,7 +37,7 @@ const MenuButton = ({ menu }: { menu: any }) => {
           [
             <MenuItem key={item} onClick={item.onClick} href={item.href}>
               <Link href={item.href}>
-                <a>{item.name}</a>
+                <Typography className={classes.menu_link}>{item.name}</Typography>
               </Link>
             </MenuItem>
           ]
